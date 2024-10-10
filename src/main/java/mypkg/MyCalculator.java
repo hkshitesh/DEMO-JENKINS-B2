ee22e2aa16290a76652186c05a26a5cf811f2548
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-10-10 14:39:47 +0530

    message added

--- src/main/java/mypkg/MyCalculator.java
+++ src/main/java/mypkg/MyCalculator.java
@@ -30,6 +30,7 @@ public class MyCalculator {
 		System.out.println("Mul is : "+calc.mul(20, 10));
 		System.out.println("Div is : "+calc.div(20, 10));
 		System.out.println("Project form SRE Class");
+		calc.gretting();
 	}
 
 }
